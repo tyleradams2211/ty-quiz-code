6cd90a81474df30453ac00d925163ddc80e55c70
tyleradams2211 <tyleradams2211@gmail.com>
2022-10-10 10:57:06 -0700

    added ids for indexing dynamic structure, added base events for dynamic creation

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,25 +1,73 @@
-const takeQuizBtn = document.getElementById('takeQuiz');
 const mainCont = document.getElementById('main-container');
-const quizCont = document.createElement('div');
+const highScores = document.getElementById('highScores');
+const currentE = document.getElementById('scriptBot');
+
+
+const takeQuizBtn = document.getElementById('takeQuiz');
+
+const questH1 = document.getElementById('questH1');
+const questP = document.getElementById('questP');
+
+const answerA = document.getElementById('A');
+const answerB = document.getElementById('B');
+const answerC = document.getElementById('C');
+const answerD = document.getElementById('D');
+
+
+// question pool
 const questions = {
     twoPlusTwo: {
         question: 'What is 2 + 2?',
-        a1: '2',
-        a2: '0',
-        a3: '4*',
-        a4: '8'
+        A: '2',
+        B: '6',
+        C: '4',
+        D: '8',
+        coorectAnswer: 'c'
+    },
+    fourPlusTwo: {
+        question: 'What is 2 + 2?',
+        A: '2',
+        B: '6',
+        C: '4',
+        D: '8',
+        coorectAnswer: 'b'
     }
 }
+// cancel button
+const cancelBtn = document.createElement('input');
+cancelBtn.type = 'button';
+cancelBtn.value = 'Cancel';
 
-quizCont.className += 'quizCont';
+//remove highscores
 
-console.log(questions.twoPlusTwo.question);
 
-takeQuizBtn.addEventListener('click', event => {
-    alert('quiz has started');
-    mainCont.remove();
-    let quizQues = document.createTextNode(questions.twoPlusTwo.question);
-    quizCont.appendChild(quizQues);
-    const currentE = document.getElementById('scriptBot');
-    document.body.insertBefore(quizCont, currentE);
+cancelBtn.addEventListener('click', event => {
+    // takes window back to original index.html
+    window.location = 'index.html'; 
 })
+// elements to create and destroy during event
+
+
+// classes and other values added to elements
+
+
+// event for starting the quiz
+takeQuizBtn.addEventListener('click', event => {
+    questH1.textContent = 'Question 1';
+    questP.textContent = questions.twoPlusTwo.question;
+    mainCont.append(cancelBtn);
+})
+
+
+
+// ref
+
+{/* 
+<label for=""><input type="checkbox">Test</label> 
+
+highScores.remove();
+
+
+
+
+*/}
